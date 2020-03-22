4aee24ea71c357a0ce20515945f9d7b7d832a6ec
YannikH <yannikhegge@gmail.com>
2020-03-22 23:02:17 +0100

    multiple lines and basic tooltip functional

--- src/d3graph.js
+++ src/d3graph.js
@@ -1,2 +1,13 @@
+export const colors = {
+  BLUE: 'steelBlue',
+  GREEN: 'DarkGreen',
+  RED: 'Red'
+}
+
 export class D3Graph {
+  constructor(label, data, color) {
+    this.label = label
+    this.data = data
+    this.color = color
+  }
 }
