22b3a78959a4e2653342b779f4a9dbe7a9fb4dc9
YannikH <yannikhegge@gmail.com>
2020-03-24 14:55:32 +0100

    UI Improvements

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import { D3Graph, colors } from './d3graph.js'
 data.getDataSet().then(data => {
   console.log(data)
   let chart = new D3Chart("#chartSvg", "#chartTooltip")
-  //for (let line of data.WHODATA) {
-  //  chart.addGraph(new D3Graph(line.confirmed, colors.BLUE))
-  //}
-  chart.addGraph(new D3Graph('Denmark', data['Denmark'].cases, colors.BLUE))
-  chart.addGraph(new D3Graph('Netherlands', data['Netherlands'].cases, colors.GREEN))
-  chart.addGraph(new D3Graph('Norway', data['Norway'].cases, colors.RED))
+  let convert = data => Object.keys(data).map(key => [key, data[key]])
+  chart.addGraph(new D3Graph('Denmark', data.cases.denmark, colors.BLUE))
+  chart.addGraph(new D3Graph('Netherlands', data.cases.netherlands, colors.GREEN))
+  chart.addGraph(new D3Graph('Norway', data.cases.norway, colors.RED))
   chart.draw()
 })
