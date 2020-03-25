7b0ed40230c4ae273fe7bf7781af41ed17f3a67c
YannikH <yannikhegge@gmail.com>
2020-03-25 16:28:21 +0100

    New data

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ data.getDataSet().then(data => {
   //for (let line of data.WHODATA) {
   //  chart.addGraph(new D3Graph(line.confirmed, colors.BLUE))
   //}
-  chart.addGraph(new D3Graph('Denmark', data.full_cases.denmark, colors.BLUE))
-  chart.addGraph(new D3Graph('Netherlands', data.cfull_ases.netherlands, colors.GREEN))
-  chart.addGraph(new D3Graph('Norway', data.full_cases.norway, colors.RED))
+  chart.addGraph(new D3Graph('Denmark', data['Denmark'].cases, colors.BLUE))
+  chart.addGraph(new D3Graph('Netherlands', data['Netherlands'].cases, colors.GREEN))
+  chart.addGraph(new D3Graph('Norway', data['Norway'].cases, colors.RED))
   chart.draw()
 })
