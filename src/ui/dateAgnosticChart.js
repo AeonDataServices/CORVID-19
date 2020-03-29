e61b3b8a17b672f3c1b3a039732af32b31a89997
YannikH <yannikhegge@gmail.com>
2020-03-29 22:41:22 +0200

    dashed lines

--- src/ui/dateAgnosticChart.js
+++ src/ui/dateAgnosticChart.js
@@ -2,7 +2,6 @@ import { dataService } from '../data/data.js'
 import { D3Chart } from '../d3rendering/d3chart.js'
 import { D3Graph, colors } from '../d3rendering/d3graph.js'
 import { Util } from '../util/utility.js'
-import { DataSelectionProvider } from './dataSelectionProvider.js'
 import { Observable } from '../util/observable.js'
 
 export class UIManager extends Observable {
@@ -12,17 +11,8 @@ export class UIManager extends Observable {
     this.chart = new D3Chart(baseID, this.hoverCallback.bind(this))
     countryProvider.subscribe(this.renderedCountriesChanged.bind(this))
     dataSelectionProvider.subscribe(this.changeData.bind(this))
-    domainProvider.subscribe(this.changeDomain.bind(this))
     this.renderedCountries = countryProvider.getSelectedCountries()
     this.dataToShow = dataSelectionProvider.getSelectedData()
-    this.domainIndices = domainProvider.getSelectedDomainIndices()
-    this.chart.changeDomain(...this.domainIndices)
-    this.chartChanged()
-  }
-
-  changeDomain(newDomainIndices) {
-    this.domainIndices = newDomainIndices
-    this.chart.changeDomain(...this.domainIndices)
     this.chartChanged()
   }
 
@@ -38,20 +28,17 @@ export class UIManager extends Observable {
     const countriesLegend = this.interface.querySelector('.chartElement')
     countriesLegend.innerHTML = ''
     for (const graph of this.chart.graphs) {
-      const icon = (graph.style.length > 1) ? 'far' : 'fas'
-      Util.appendElement(countriesLegend, 'h6', `<i style="color: ${graph.color}" class="${icon} fa-square"></i> ${graph.label}`)
+      Util.appendElement(countriesLegend, 'h6', `<i style="color: ${graph.color}" class="fas fa-square"></i> ${graph.label}`)
     }
   }
 
   drawCountry(country) {
-    let dataIndex = 0
     for (let dataProcessor of this.dataToShow) {
       let label = dataProcessor.getLabel(country)
       if (this.chart.graphExists(label)) continue
       let countryData = dataProcessor.getData(country)
       let color = Util.colors[this.chart.graphs.length]
-      let stroke = (dataIndex > 0) ? ["stroke-dasharray", ("3, 3")] : ['']
-      this.chart.addGraph(new D3Graph(label, countryData, color, stroke))
+      this.chart.addGraph(new D3Graph(label, countryData, color))
       this.chart.draw()
       let div = document.createElement('div')
       div.className = "countryOption"
@@ -59,7 +46,6 @@ export class UIManager extends Observable {
       div.querySelector('.delete').onclick = (event => {
         this.removeCountry(...(event.target.getAttribute('data-value')).split(','), event.target)
       })
-      dataIndex++
     }
   }
 
