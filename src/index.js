9b1142528c0441129a23502e8e3553a0fc39160a
YannikH <yannikhegge@gmail.com>
2020-04-02 20:54:27 +0200

    Color picker on charts

--- src/index.js
+++ src/index.js
@@ -12,14 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
   google.charts.setOnLoadCallback(() => {
     dataService.isDataInitialized().then(() => {
       const countrySelector = new CountrySelector('#mainDisplay .countries-list')
-      const domainProvider = new DomainProvider('.dateRange-slider')
+      //const domainProvider = new DomainProvider('.dateRange-slider')
       new GoogleChart('#chart-total-cases', countrySelector, 'Total confirmed cases','getTotalCasesByDate')
       new GoogleChart('#chart-new-cases', countrySelector, 'New cases','getNewCasesByDate')
       new GoogleChart('#chart-growth-rate', countrySelector, 'Growth rate','getGrowthRateByDate', {
         vAxis:
         {
             logScale: true,
-            minValue: 0
+            viewWindow: {
+              min: 0,
+              max: 50
+            }
         }
       })
       new GoogleChart('#chart-death-rate', countrySelector, 'Death rate','getDeathRateByDate')
