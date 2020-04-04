29e20492bacd6a9d3dfaa288fc08fcc189861a39
YannikH <yannikhegge@gmail.com>
2020-04-04 11:45:12 +0200

    Race condition removed, loading bar added

--- src/index.js
+++ src/index.js
@@ -7,30 +7,35 @@ import { CasesFromDeathsProcessor } from './data/casesFromDeathsProcessor.js'
 import { InputProvider } from './ui/inputProvider.js'
 import { DelayedDetectionCasesProcessor } from './data/delayedDetectionProcessor.js'
 import { GoogleChart } from './ui/googleChart.js'
-document.addEventListener('DOMContentLoaded', () => {
-  google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(() => {
-    dataService.isDataInitialized().then(() => {
-      const countrySelector = new CountrySelector('#mainDisplay .countries-list')
-      //const domainProvider = new DomainProvider('.dateRange-slider')
-      new GoogleChart('#chart-total-cases', countrySelector, 'Total confirmed cases','getTotalCasesByDate')
-      new GoogleChart('#chart-new-cases', countrySelector, 'New cases','getNewCasesByDate')
-      new GoogleChart('#chart-growth-rate', countrySelector, 'Growth rate','getGrowthRateByDate', {
-        vAxis:
-        {
-            logScale: true
-        }
-      })
-      new GoogleChart('#chart-death-rate', countrySelector, 'Death rate','getDeathRateByDate')
-      
-      const mainMap = new google.visualization.GeoChart(document.getElementById('mainMap'))
-      mainMap.draw(dataService.getAllCountriesInfectionDensity(), {
-        colorAxis: {colors: ['white', 'red']},
-        datalessRegionColor: '#adadad',
-        legend: 'none'
-      })
+import { loadingScreen } from './ui/loadingScreen.js'
 
-      M.Tabs.init(document.querySelector('.tabs'), {})
+loadingScreen.startLoading()
+
+loadingScreen.updateText('Loading DOM')
+document.addEventListener('DOMContentLoaded', () => {
+  loadingScreen.updateText('DOM loaded')
+  loadingScreen.updateText('Initializing Data')
+  dataService.isDataInitialized().then(() => {
+    loadingScreen.finishLoading()
+    const countrySelector = new CountrySelector('#mainDisplay .countries-list')
+    //const domainProvider = new DomainProvider('.dateRange-slider')
+    new GoogleChart('#chart-total-cases', countrySelector, 'Total confirmed cases','getTotalCasesByDate')
+    new GoogleChart('#chart-new-cases', countrySelector, 'New cases','getNewCasesByDate')
+    new GoogleChart('#chart-growth-rate', countrySelector, 'Growth rate','getGrowthRateByDate', {
+      vAxis:
+      {
+          logScale: true
+      }
     })
+    new GoogleChart('#chart-death-rate', countrySelector, 'Death rate','getDeathRateByDate')
+    
+    const mainMap = new google.visualization.GeoChart(document.getElementById('mainMap'))
+    mainMap.draw(dataService.getAllCountriesTable('Rate of new cases(%) past 5 days', 'getRecentGrowth'), {
+      colorAxis: {colors: ['white', 'red']},
+      datalessRegionColor: '#adadad',
+      legend: 'none'
+    })
+
+    M.Tabs.init(document.querySelector('.tabs'), {})
   })
 })
--- src/models/country.js
+++ src/models/country.js
@@ -1,23 +1,18 @@
+import { Util } from "../util/utility.js"
+
 export class Country {
     constructor(name, color, data) {
         this.name = name
         this.color = color
         this.baseData = data
         this.processData()
+        console.log(this)
     }
 
     processData() {
-        this.getOutbreakStartIndex()
+        this.generateDayReferenceData()
+        this.generateAssumedRecoveriesData()
         const dataToExtract = Object.keys(this.baseData)
-        let outbreakStartTable = []
-        for (let index = 0; index < this.baseData.casesPct.length; index++) {
-            outbreakStartTable.push((index < this.outbreakStartIndex) ? 0 : index - this.outbreakStartIndex)
-        }
-        const processedData = this.baseData.cases.map((data, i) => [data[0], outbreakStartTable[i]])
-        this.dayReference = new google.visualization.DataTable()
-        this.dayReference.addColumn('datetime', 'Date')
-        this.dayReference.addColumn('number', 'Day')
-        this.dayReference.addRows(processedData)
         for (const dataName of dataToExtract) {
             const data = new google.visualization.DataTable()
             data.addColumn('datetime', 'Date')
@@ -28,8 +23,46 @@ export class Country {
     }
 
     getOutbreakStartIndex() {
+        if (this.outbreakStartIndex) return this.outbreakStartIndex
         const index = this.baseData.casesPct.findIndex(kv => kv[1] > 10)
         this.outbreakStartIndex = index
+    }
+
+    generateDayReferenceData() {
+        this.getOutbreakStartIndex()
+        this.generateAssumedRecoveriesData()
+        let outbreakStartTable = []
+        for (let index = 0; index < this.baseData.casesPct.length; index++) {
+            outbreakStartTable.push((index < this.outbreakStartIndex) ? 0 : index - this.outbreakStartIndex)
+        }
+        const processedData = this.baseData.cases.map((data, i) => [data[0], outbreakStartTable[i]])
+        this.dayReference = new google.visualization.DataTable()
+        this.dayReference.addColumn('datetime', 'Date')
+        this.dayReference.addColumn('number', 'Day')
+        this.dayReference.addRows(processedData)
+    }
+
+    generateAssumedRecoveriesData(recoveryTime = 14) {
+        const newCases = this.baseData['newCases']
+        const newDeaths = this.baseData['newDeaths']
+        const assumedRecoveries = []
+        for (let index = newCases.length - 1; index >= 0; index--) {
+            const date = newCases[index][0]
+            if (index < recoveryTime) {
+                assumedRecoveries[index] = [date, 0]
+                continue
+            }
+            const cases = newCases[index - recoveryTime][1]
+            assumedRecoveries[index] = [date, cases]
+        }
+        this.assumedRecoveries = Util.gerateDataTable([['datetime', 'Date'], ['number', this.name]], assumedRecoveries)
+        let totalRecoveries = 0
+        const activeCases = this.baseData['cases'].map(([date, val], index) => {
+            totalRecoveries = totalRecoveries + assumedRecoveries[index][1]
+            return [date, val - totalRecoveries]
+        })
+        console.log(this.name, totalRecoveries)
+        this.activeCases = Util.gerateDataTable([['datetime', 'Date'], ['number', this.name]], activeCases)
     }
 
     getTotalCasesByDate() {
@@ -52,6 +85,27 @@ export class Country {
     }
     getTotalCases() {
         return this.baseData.cases[this.baseData.cases.length - 1][1]
+    }
+    getAssumedRecoveries() {
+        return this.assumedRecoveries
+    }
+    getActiveCases() {
+        return this.activeCases
+    }
+    getCaseDensity() {
+        const pop100k =  (this.data.population) / 100000
+        return this.getTotalCases() / pop100k
+    }
+    getRecentGrowth(start = 5, days = 5) {
+        const casesPct = this.baseData['casesPct']
+        const daysToAverage = casesPct.slice(casesPct.length - start, casesPct.length - start + days).map(data => data[1])
+        return daysToAverage.reduce((a, b) => a + b, 0) / days
+    }
+    getRecentGrowthChange(dayCount = 7) {
+        const firstGrowth = this.getRecentGrowth(dayCount, dayCount)
+        const secondGrowth = this.getRecentGrowth(2*dayCount, dayCount)
+        console.log(this.name, firstGrowth, secondGrowth)        
+        return firstGrowth - secondGrowth
     }
 
     getName() {
