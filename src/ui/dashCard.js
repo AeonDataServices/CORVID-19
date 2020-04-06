6f43b6ea7b47cd47742c8048cee528ec4cec9436
spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-04-06 17:37:05 +0200

    responsiveness for smaller and medium screens

--- src/ui/dashCard.js
+++ src/ui/dashCard.js
@@ -1,9 +1,9 @@
 import { Util } from "../util/utility.js"
 
 export class DashCard {
-	constructor(containerID, title, text, actions) {
+	constructor(containerID, title, text, actions, className='') {
 		const container = document.querySelector(containerID)
-		this.element = Util.appendElement(container, 'div', '', 'card')
+		this.element = Util.appendElement(container, 'div', '', `card ${className}`)
 		this.content = Util.appendElement(this.element, 'div', '', 'card-content')
 		Util.appendElement(this.content, 'span', title, 'card-title')
 		Util.appendElement(this.content, 'p', text)
