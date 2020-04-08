4f6910d6e1c26bf44865c76f8e274a95a7b3d411
spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2020-04-08 15:47:57 +0200

    initial styling of colorpicker

--- src/ui/countrySelector.js
+++ src/ui/countrySelector.js
@@ -16,15 +16,14 @@ export class CountrySelector extends Observable {
       const section = Util.appendElement(this.listDiv, 'p', '')
       const label = Util.appendElement(section, 'label', '')
       const input = Util.appendElement(label, 'input', '')
+      const colorLabel = Util.appendElement2(section, 'label','', 'colorLabel', `colorPicker${country.baseData.alpha3}`)
       input.addEventListener('change', this.changeSelectionEvent.bind(this))
       input.setAttribute('type', 'checkbox')
       if (this.isCountrySelected(country.getName())) input.setAttribute('checked', 'checked')
       input.setAttribute('class', 'filled-in')
       input.setAttribute('data-country', country.getName())
       Util.appendElement(label, 'span', `${country.getName()}`)
-      //const picker = Util.appendElement(section, 'span', `<i style="color: ${country.getColor()}" class="fas fa-square"></i>`)
-      //picker.addEventListener('click', Util.colorPicker.bind(null, [this]))
-      const picker = Util.appendElement(section, 'input', ``)
+      const picker = Util.appendElement(colorLabel, 'input', ``)
       picker.setAttribute('type', 'color')
       picker.setAttribute('value', country.getColor())
       picker.addEventListener('change', this.changeColorEvent.bind(this, country))
@@ -39,6 +38,10 @@ export class CountrySelector extends Observable {
   changeColorEvent(countryName, event) {
     const color = event.target.value
     countryName.color = color
+    console.log(countryName)
+    console.log(countryName.name)
+    let countryLabel = document.querySelector(`#colorPicker${countryName.baseData.alpha3}`)
+    countryLabel.style.backgroundColor = color
     this.notifyObservers(this.selectedCountries)
   }
 
